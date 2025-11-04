7ed037985103eec073458cffacf6673de454f258
tauqirkhan <tauqirkhann.11@gmail.com>
2025-11-04 16:58:21 +0530

    feat: add homepage

---
+++ components/SearchBar.tsx
@@ -0,0 +1,73 @@
+"use client"
+
+import { parse } from 'path'
+import React, { FormEvent, useState } from 'react'
+
+const isValidAmazonProductURL = (url: string): boolean => {
+    try {
+        const parseUrl = new URL(url)
+        const hostname = parseUrl.hostname
+
+        if(
+            hostname.includes('amazon.com') ||
+            hostname.includes('amazon.') ||
+            hostname.startsWith('amazon')
+        ) {
+            return true;
+        }
+        
+    } catch (error) {
+        return false;
+    }
+
+    return false;
+}
+
+const SearchBar = () => {
+    const [searchPrompt, setSearchPrompt] = useState('');
+    const [isLoading, setIsloading] = useState(false)
+
+    const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+
+        const isValidLink = isValidAmazonProductURL(searchPrompt)
+
+        if(!isValidLink) return alert('Please provide a valid Amazon link')
+
+        try {
+            setIsloading(true)
+
+            //scrape the product page
+            
+        } catch (error) {
+            console.error(error)
+        } finally {
+            setIsloading(false)
+        }
+    }
+
+    return (
+            <form 
+                className='flex flex-wrap gap-4 mt-12'
+                onSubmit={handleSubmit}
+            >
+                <input 
+                    type="text"
+                    placeholder='Enter product link'
+                    className='searchbar-input'
+                    onChange={(e) => setSearchPrompt(e.target.value)}
+                    value={searchPrompt}
+                />
+
+                <button    
+                    type='submit' 
+                    className='searchbar-btn'
+                    disabled={searchPrompt === ''}    
+                >
+                    {isLoading ? 'Searching...': 'Search'}
+                </button>
+            </form>
+        )
+}
+
+export default SearchBar
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,55 @@
 import React from 'react'
+import Image from 'next/image'
+import SearchBar from '../../components/SearchBar'
+import HeroCarousel from '../../components/HeroCarousel'
 
 const Home = () => {
   return (
-    <div>page</div>
+    <>
+    {/* py-24 */}
+    <section className='px-6  md:px-20 '>
+      <div className='flex max-xl:flex-col gap-16'>
+        <div className='flex flex-col justify-center'>
+          <p className='small-text'>
+            Smart Shopping Starts Here:
+            <Image 
+              src="/assets/icons/arrow-right.svg"
+              alt="arrow-right"
+              width={16}
+              height={16}
+            />
+          </p>
+
+          <h1 className='head-text'>
+            Unleash the Power of
+            <span className='text-primary'> PriceWise</span>
+          </h1>
+
+          <p className='mt-6'>
+            Powerful, self-serve product and growth 
+            analytics to help you convert, engage, and 
+            retain more.
+          </p>
+
+          <SearchBar />
+        </div>
+
+       <HeroCarousel />
+      </div>
+    </section>
+
+    <section className='trending-section'>
+      <h2 className='section-text'>Trending</h2>
+
+      <div className='flex flex-wrap gap-x-8 gap-y-16'>
+        {['Iphone Air', 'Book', 'Shoe'].map((product) => (
+          <div key={product}>
+            {product}
+          </div>
+        ))}
+      </div>
+    </section>
+    </>
   )
 }
 
